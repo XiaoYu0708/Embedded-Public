c9f4798f190def97ff5e8732e8efdf040573d86e
XiaoYu <68182187+XiaoYu0708@users.noreply.github.com>
2022-11-17 10:02:49 +0800

    Add files via upload

---
+++ WriteSingleField/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "C306-34 2917"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "12345678"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1928514		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "GGMXAP8XBGIK3M3E"   // replace XYZ with your channel write API Key
