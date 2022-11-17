a8bc7033411f266c2ad781ef9218bdee9073f690
XiaoYu <68182187+XiaoYu0708@users.noreply.github.com>
2022-11-17 20:50:27 +0800

    Update sketch_nov17a.ino

--- sketch_nov17a.ino
+++ sketch_nov17a.ino
@@ -52,40 +52,49 @@ void loop() {
 
     for (JsonObject field : doc["fields"].as<JsonArray>()) {
 
-      const char* field_id = field["id"]; // "site", "county", "pm25", "datacreationdate", "itemunit"
-      const char* field_type = field["type"]; // "text", "text", "text", "text", "text"
+      const char* field_id = field["id"];      // "site", "county", "pm25", "datacreationdate", "itemunit"
+      const char* field_type = field["type"];  // "text", "text", "text", "text", "text"
 
-      const char* field_info_label = field["info"]["label"]; // "測站名稱", "縣市名稱", "細懸浮微粒濃度", "資料建置日期", "測項單位"
-
+      const char* field_info_label = field["info"]["label"];  // "測站名稱", "縣市名稱", "細懸浮微粒濃度", "資料建置日期", "測項單位"
     }
 
-    const char* resource_id = doc["resource_id"]; // "c1f31192-babd-4105-b880-a4c2e23a3276"
+    const char* resource_id = doc["resource_id"];  // "c1f31192-babd-4105-b880-a4c2e23a3276"
 
-    const char* extras_api_key = doc["__extras"]["api_key"]; // "e8dd42e6-9b8b-43f8-991e-b3dee723a52d"
+    const char* extras_api_key = doc["__extras"]["api_key"];  // "e8dd42e6-9b8b-43f8-991e-b3dee723a52d"
 
-    bool include_total = doc["include_total"]; // true
-    const char* total = doc["total"]; // "78"
-    const char* resource_format = doc["resource_format"]; // "object"
-    const char* limit = doc["limit"]; // "1000"
-    const char* offset = doc["offset"]; // "0"
+    bool include_total = doc["include_total"];             // true
+    const char* total = doc["total"];                      // "78"
+    const char* resource_format = doc["resource_format"];  // "object"
+    const char* limit = doc["limit"];                      // "1000"
+    const char* offset = doc["offset"];                    // "0"
 
     const char* links_start = doc["_links"]["start"];
     const char* links_next = doc["_links"]["next"];
 
     for (JsonObject record : doc["records"].as<JsonArray>()) {
 
-      const char* record_site = record["site"]; // "大城", "富貴角", "麥寮", "關山", "馬公", "金門", "馬祖", "埔里", "復興", ...
-      const char* record_county = record["county"]; // "彰化縣", "新北市", "雲林縣", "臺東縣", "澎湖縣", "金門縣", "連江縣", "南投縣", ...
-      const char* record_pm25 = record["pm25"]; // "9", "10", "14", "4", "9", "7", "8", "13", "18", "21", ...
-      const char* record_datacreationdate = record["datacreationdate"]; // "2022-11-17 17:00", "2022-11-17 ...
-      const char* record_itemunit = record["itemunit"]; // "μg/m3", "μg/m3", "μg/m3", "μg/m3", "μg/m3", ...
-      if(record["site"]=="臺南"){
+      const char* record_site = record["site"];                          // "大城", "富貴角", "麥寮", "關山", "馬公", "金門", "馬祖", "埔里", "復興", ...
+      const char* record_county = record["county"];                      // "彰化縣", "新北市", "雲林縣", "臺東縣", "澎湖縣", "金門縣", "連江縣", "南投縣", ...
+      const char* record_pm25 = record["pm25"];                          // "9", "10", "14", "4", "9", "7", "8", "13", "18", "21", ...
+      const char* record_datacreationdate = record["datacreationdate"];  // "2022-11-17 17:00", "2022-11-17 ...
+      const char* record_itemunit = record["itemunit"];                  // "μg/m3", "μg/m3", "μg/m3", "μg/m3", "μg/m3", ...
+      int PM = atoi(record_pm25);
+      if (record["site"] == "臺南") {
         Serial.print("PM2.5：");
         Serial.println(record_pm25);
+        if (PM >= 251) {
+          Serial.println("等級為紫色");
+        } else if (PM >= 55) {
+          Serial.println("等級為紅色");
+        } else if (PM >= 36) {
+          Serial.println("等級為橘色");
+        } else if (PM >= 16) {
+          Serial.println("等級為黃色");
+        } else if (PM >= 0) {
+          Serial.println("等級為綠色");
+        }
       }
     }
-
-
   }
   http.end();
   delay(10000);
